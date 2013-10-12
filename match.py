f7bf102a71ef2237ca496f65db54da55c8518ac5
Sameer <schauhan19@gmail.com>
2013-10-12 18:53:19 -0400

    Changed one test photo. Made a useless class

---
+++ match.py
@@ -0,0 +1,11 @@
+class matchChamp:
+    def __init__(self):
+        self.title = "what"
+    def matchChamp(self,pathToTits):
+        print("Matching Character: " + pathToTits)
+
+
+
+if __name__ == "__main__":
+    m = matchChamp()
+    m.matchChamp("img/thresh1.png")
